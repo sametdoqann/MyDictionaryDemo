e958d057dfc303c15f141b68549ba152edf503f1
sametdoqann <sametdoqann-s.doqann1570@gmail.com>
2021-01-23 19:34:18 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            
+            myDictionary.Add(100, "Samet DOGAN");
+            myDictionary.Add(101, "Engin DEMİROG");
+
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
